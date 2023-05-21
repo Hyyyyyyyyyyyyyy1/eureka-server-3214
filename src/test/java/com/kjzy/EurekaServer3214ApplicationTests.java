a8856732f87c8977b04ebec3a6b228fa08d20b9b
hyy <2819341840@qq.com>
2023-05-21 22:52:46 +0800

    111

---
+++ src/test/java/com/kjzy/EurekaServer3214ApplicationTests.java
@@ -0,0 +1,17 @@
+package com.kjzy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootTest
+@EnableEurekaServer
+@RestController
+public class EurekaServer3214ApplicationTests {
+
+    @Test
+public     void contextLoads() {
+    }
+
+}
